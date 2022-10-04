267bc362ecfe7236e3baef003198581b4fd509c5
THE WORLD BEST <100334196+azimkg@users.noreply.github.com>
2022-10-04 23:15:45 +0600

    deploy

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
-app.use(express.static("public"));
+const middlewares = express.defaults({
+  static: "./build",
+});
+app.use(middlewares);
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 app.listen(port, () =>
   console.log(`Server is running on: http://localhost:${port}`)
